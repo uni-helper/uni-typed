71a0cbddec66afd0101c283792ef0ef761ae0022
Panpan <wurui.dev@gmail.com>
2026-06-26 22:04:54 +0800

    chore: naming

--- packages/uni-app-components/src/components/Ad/index.ts
+++
@@ -1,6 +0,0 @@
-import type { AdInstance } from "@uni-helper/uni-app-types";
-import Ad from "./ad.vue";
-
-export default Ad;
-
-export { Ad, type AdInstance };
--- packages/uni-app-components/src/components/Input/index.ts
+++
@@ -1,6 +0,0 @@
-import type { InputInstance } from "@uni-helper/uni-app-types";
-import Input from "./input.vue";
-
-export default Input;
-
-export { Input, type InputInstance };
