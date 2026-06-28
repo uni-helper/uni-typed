1ad819bdfe591542dc32fb15ad2380eb27f04c18
Panpan <wurui.dev@gmail.com>
2026-06-28 13:59:47 +0800

    feat: introduce uni-cloud-components

---
+++ packages/uni-cloud-components/src/components/unicloud-db/index.ts
@@ -0,0 +1,6 @@
+import type { UnicloudDbInstance } from "@uni-helper/uni-cloud-types";
+import UnicloudDb from "./unicloud-db.vue";
+
+export default UnicloudDb;
+
+export { UnicloudDb, type UnicloudDbInstance };
---
+++ packages/uni-cloud-components/src/components/unicloud-db/unicloud-db.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { UnicloudDbProps } from "@uni-helper/uni-cloud-types";
+
+defineOptions({
+  name: "UnicloudDb",
+});
+
+const props = defineProps<UnicloudDbProps>();
+</script>
+
+<template>
+  <unicloud-db v-bind="props">
+    <slot></slot>
+  </unicloud-db>
+</template>
